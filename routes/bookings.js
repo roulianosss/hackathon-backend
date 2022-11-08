5a693a2c7a9650f5d5469ee0f24922704ae63f4e
Roulianosss <julien.ortscheid@gmail.com>
2022-11-08 18:50:09 +0100

    test

--- routes/bookings.js
+++ routes/bookings.js
@@ -1,9 +1,47 @@
 var express = require('express');
 var router = express.Router();
+const Cart = require('../models/carts')
+const Booking = require('../models/bookings')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  Booking.find().then(data => res.json(data ))
 });
+
+router.get('/add', function(req, res, next) {
+  Cart.find().then(allCartItem => {
+    allCartItem.forEach(item => {
+      const newItem = new Booking({
+        departure: item.departure,
+        arrival: item.arrival,
+        date: item.date,
+        price: item.price
+      })
+      newItem.save().then(data => console.log(data))
+
+    })
+
+    Cart.deleteMany({}).then(data => res.json(data))
+
+  })
+});
+
+router.delete('/delete', (req, res) => {
+  Booking.deleteMany({}).then(data => res.json(data))
+})
+
+// router.get('/add', (req, res) => {
+//   Cart.find().then(data => {
+//     data.forEach(item => {
+//       const newItem = new Booking({
+//         departure: item.departure,
+//         arrival: item.arrival,
+//         date: item.date,
+//         price: item.price
+//       })
+//       newItem.save().then(data => res.json(data))
+//     })
+//   })
+// })
 
 module.exports = router;
