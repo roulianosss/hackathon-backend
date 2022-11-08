92cb11305af36807ad0ee89097b853cd8c797b5c
Roulianosss <julien.ortscheid@gmail.com>
2022-11-08 12:11:47 +0100

    new routes trips

--- routes/trips.js
+++ routes/trips.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const Trip = require('../models/trips')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  Trip.find().then(allTrips => res.send(allTrips))
+  ;
 });
+
+router.get('/new', (req, res) => {
+    Trip.find().then(allTrips => {
+        console.log(allTrips[0].date.toLocaleString())
+        const result = allTrips.filter(trip => trip.departure === req.body.departure && trip.arrival === req.body.arrival && trip.date.toLocaleString().split(' ')[0] === req.body.date )
+        res.json(result)
+    })
+})
 
 module.exports = router;
