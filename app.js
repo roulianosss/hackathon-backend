c11914bb3def567e880c7b4d6aba00acef291e80
Olivier Paly <olivier.paly@gmail.com>
2022-11-08 11:18:46 +0100

    v1 backend - tickethack MongoDBB + models/connection.js + models/trips.js + require/models/connection + mongoose install - status DB connected

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+//require (“./models/connections")
+require("./models/connection");
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
 const app = express();
 
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 module.exports = app;
---
+++ models/connection.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const connectionString =
+  "mongodb+srv://olivus:CGshpbIrui4dmM3O@cluster0.zsw9eh4.mongodb.net/tickethack";
+
+mongoose
+  .connect(connectionString, { connectTimeoutMS: 2000 })
+  .then(() => console.log("Database connected"))
+  .catch((error) => console.error(error));
+
+module.exports = connectionString;
